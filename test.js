c6ba8b3786396bec384e6b0cafde96fa22811d26
walnutdust <36098015+walnutdust@users.noreply.github.com>
2018-02-12 17:25:48 +0800

    Commit

---
+++ test.js
@@ -0,0 +1,123 @@
+let curr = "";
+let num1 = 0;
+let num2 = 0;
+let decimal = false;
+let flag = false;
+let operation = 0;
+let toUpdate = "";
+
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+const allclear = document.querySelector('#all-clear');
+const dot = document.querySelector('#dot');
+
+const opbuttons = document.querySelectorAll('.opbuttons');
+const nbuttons = document.querySelectorAll('.nbuttons');
+
+nbuttons.forEach(function (element){
+  element.addEventListener('click', function (e){
+    console.log(e);
+    if(operation == 0) {
+      if(!decimal) num1 = num1*10 + parseInt(element.childNodes[0].nodeValue);
+      else num1 = parseFloat(num1.toString() + element.childNodes[0].nodeValue);
+
+      if(flag) num1 /=10;
+
+      flag = false;
+      console.log(num1);
+      update2("              " +  num1.toString());
+    }
+    else {
+      num2 = num2* 10 + parseInt(element.childNodes[0].nodeValue);
+      if(decimal) num2/=10;
+      update2("              " + num2.toString());
+    }
+  })
+})
+
+opbuttons.forEach(function (element){
+  element.addEventListener('click', function (e){
+    console.log(e);
+    decimal = false;
+
+    if(e.target.id == 1) curr = " + ";
+    if(e.target.id == 2) curr = " - ";
+    if(e.target.id == 3) curr = " × ";
+    if(e.target.id == 4) curr = " ÷ ";
+
+    executeOperation();
+    operation = parseInt(e.target.id);
+  })
+})
+
+function update1(str){
+  document.getElementById('prev').innerHTML = str.substring(str.length-20, str.length);
+}
+
+function update2(str){
+  document.getElementById('screentext').innerHTML = str.substring(str.length-14, str.length);
+}
+
+function executeOperation(){
+  console.log(num1 + curr + num2);
+  if(operation == 1) num1 += num2;
+  if(operation == 2) num1 -= num2;
+  if(operation == 3 && num2 != 0) num1 *= num2;
+  if(operation == 4 && num2 != 0) num1 /= num2;
+
+  num2 = 0;
+  flag = false;
+
+  update1("              " + num1.toPrecision(10).toString() + curr);
+  update2("              0");
+
+}
+
+allclear.addEventListener('click', function (e){
+  console.log(e);
+  operation = 0;
+  num1 = 0;
+  num2 = 0;
+  decimal = false;
+  flag = false;
+  document.getElementById('prev').innerHTML = "0";
+  document.getElementById('screentext').innerHTML = "0";
+  }
+)
+
+clear.addEventListener('click', function (e){
+  console.log(e);
+  if(operation == 0){
+    if(decimal) num1 = parseFloat(num1.toString().substring(0,-2));
+    else num1 = Math.trunc(num1/10);
+    update2("              " + num1.toString());
+  }
+  else if (num2 != 0){
+    if(decimal) num2 = parseFloat(num2.toString().substring(0,-2));
+    num2 = Math.trunc(num2/10);
+    update2("              " +num1.toString() + curr + num2.toString());
+  }
+  }
+)
+
+equals.addEventListener('click', function (e){
+  console.log(e);
+  curr = "";
+  executeOperation();
+  operation = 0;
+  }
+)
+
+dot.addEventListener('click', function (e){
+  console.log(e);
+  if(!decimal) {
+    curr = ".";
+    decimal = true;
+    num2 = 0;
+    flag = false;
+    update2("              " + num1.toPrecision(10).toString() + curr);
+    operation = 0;
+    flag = true;
+  }
+  }
+)
